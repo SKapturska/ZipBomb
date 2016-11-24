140d77c8cf20453c06c07f19c4e419b6ad32c84d
SKapturska <skapturska@us.edu.pl>
2016-11-24 12:59:32 +0100

    Koniec

--- src/zipbomb/FileCreateThread.java
+++ src/zipbomb/FileCreateThread.java
@@ -1,14 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package zipbomb;
 
-/**
- *
- * @author Sandi
- */
-public class FileCreateThread {
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Random;
+
+public class FileCreateThread implements Runnable{
+    String fileName;
+    
+    public FileCreateThread(String fileName) {
+        this.fileName = fileName;
+    }
+
+    @Override
+    public void run() {
+        try{
+            DataOutputStream dataOut = new DataOutputStream(new FileOutputStream(fileName));
+            Random r = new Random();
+            for(int i = 0;i < 1000000;i++){
+                dataOut.writeByte(r.nextInt(256));
+            }
+            dataOut.close();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
     
 }
